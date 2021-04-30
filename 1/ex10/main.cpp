4d13bafcbfdacf201f0efe0cc21ca4bfd768ff8d
mattia <isao@hotmail.it>
2021-05-01 01:13:34 +0200

    second option

--- 1/ex10/main.cpp
+++ 1/ex10/main.cpp
@@ -1,14 +1,48 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <stdlib.h>
+
+// int main(int ac, const char **av)
+// {
+// 	std::string line;
+// 	if (ac == 1)
+// 	{
+// 		while (getline(std::cin, line))
+// 			std::cout << line << std::endl;
+// 		exit(0);
+// 	}
+// 	for (int i = 1; i < ac; i++)
+// 	{
+// 		std::ifstream in;
+// 		in.open(av[i]);
+// 		if (!in.is_open())
+// 		{
+// 			std::cerr << "cato9tails: " << av[i] << ": File o directory non esistente" << std::endl;
+// 			continue ;
+// 		}
+// 		while (getline(in, line))
+// 			std::cout << line << std::endl;
+// 		in.close();
+// 	}
+// 	exit(0);
+// }
+
 
 int main(int ac, const char **av)
 {
 	std::string line;
+	std::stringstream ss;
+	char c = 0;
 	if (ac == 1)
 	{
-		while (getline(std::cin, line))
-			std::cout << line << std::endl;
+		std::cin.rdbuf();
+		c = char(std::cin.get());
+		while (c != EOF)
+		{
+			 std::cout << c;
+			 c = char(std::cin.get());
+		}
 		exit(0);
 	}
 	for (int i = 1; i < ac; i++)
@@ -20,8 +54,14 @@ int main(int ac, const char **av)
 			std::cerr << "cato9tails: " << av[i] << ": File o directory non esistente" << std::endl;
 			continue ;
 		}
-		while (getline(in, line))
-			std::cout << line << std::endl;
+		in.rdbuf();
+		c = char(in.get());
+		while (c != EOF)
+		{
+			std::cout << c;
+			c = char(in.get());
+		}
+		in.close();
 	}
 	exit(0);
 }
