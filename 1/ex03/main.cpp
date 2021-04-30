5e3636a60bf42bf6e4e82c2a4f4ae89a663a5559
Mattia Cossu <mcossu@e3r8p5.42roma.it>
2021-04-30 15:01:45 +0200

    ZombieHorde

--- 1/ex03/main.cpp
+++ 1/ex03/main.cpp
@@ -1,22 +1,15 @@
-#include "ZombieEvent.hpp"
+#include "ZombieHorde.hpp"
 
 int main()
 {
-	ZombieEvent ze0, ze1, ze2;
-	Zombie *z0, *z1, *z2;
-	ze0.setZombieType(Generic);
-	ze1.setZombieType(Voodoo);
-	ze2.setZombieType(Runner);
-	z0 = ze0.newZombie("Gijsje");
-	z0->announce();
-	delete z0;
-	z1 = ze1.newZombie("Naminex");
-	z1->announce();
-	delete z1;
-	z2 = ze2.newZombie("Mysen");
-	z2->announce();
-	delete z2;
-	ze0.randomChump();
-	ze1.randomChump();
-	ze2.randomChump();
+	int n = 0;
+	while (n <= 0)
+	{
+		std::cout << "Please Insert the number of Zombie that you want" << std::endl;
+		std::cin >> n;
+		if (n <= 0)
+			std::cout << "Please insert a positive not 0 number" << std::endl;
+	}
+	ZombieHorde zh = ZombieHorde(n);
+	zh.announce();
 }
