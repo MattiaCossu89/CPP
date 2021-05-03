c8dc4c2e95a799f1824cda364497287e40f9b02b
mattia <isao@hotmail.it>
2021-05-03 13:07:38 +0200

    FragTrap inherit ClpTrap

---
+++ 3/ex02/ClapTrap.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#define UI unsigned int
+#define str std::string
+#define NN 2
+#define NS 2
+#define NMB 1
+#define NMD 1
+#define NVDE 5
+
+#include <string>
+#include <iostream>
+
+class ClapTrap
+{
+	private:
+		str	name;
+		UI	life;
+		UI	maxlife;
+		UI	mc2;
+		UI	maxmc2;
+		UI	level;
+		UI	meleeDmg;
+		UI	rangedDmg;
+		UI	armor;
+
+		static const str	names[NN];
+		static const str	surNames[NS];
+		static bool			first;
+
+		static const str	randName();
+		ClapTrap();
+	public:
+		ClapTrap(const str &name, UI life, UI maxlife, UI mc2, UI maxmc2, UI level, UI meleeDmg, UI rangedDmg, UI armor);
+		ClapTrap(UI life_, UI maxlife_, UI mc2_, UI maxmc2_, UI level_, UI meleeDmg_, UI rangedDmg_, UI armor_);
+		ClapTrap(const ClapTrap &cpy);
+		ClapTrap &operator=(const ClapTrap &cpy);
+		~ClapTrap();
+		virtual void	rangedAttack(ClapTrap &target) const = 0;
+		virtual void	meleeAttack(ClapTrap &target) const = 0;
+		int		takeDamage(UI &amount);
+		int		beRepaired(UI &amount);
+		bool	useEnergy(UI amount);
+		str		getName() const;
+		UI		getRangedDmg() const;
+		UI		getMeleeDmg() const;
+};
---
+++ 3/ex02/ScavTrap.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#define UI unsigned int
+#define str std::string
+#define NN 2
+#define NS 2
+#define NMB 1
+#define NMD 1
+#define NVDE 5
+
+#include "ClapTrap.hpp"
+
+#include <string>
+#include <iostream>
+
+class ScavTrap : public virtual ClapTrap
+{
+	private:
+		static const str	challenges[NVDE];
+	public:
+		ScavTrap();
+		ScavTrap(const str &name);
+		ScavTrap(const ScavTrap &cpy);
+		ScavTrap &operator=(const ScavTrap &cpy);
+		~ScavTrap();
+		void	rangedAttack(str const &target) const;
+		void	meleeAttack(str const &target) const;
+		void	challengeNewcomer(str const &target);
+		void	takeDamage(UI amount);
+		void	beRepaired(UI amount);
+		str		getName();
+};
