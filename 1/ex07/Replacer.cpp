8c6fce017df4c342a2a9e362acf82fa552d7ee2d
Mattia Cossu <mcossu@e3r8p5.42roma.it>
2021-05-04 18:38:33 +0200

    ciao

--- 1/ex07/Replacer.cpp
+++ 1/ex07/Replacer.cpp
@@ -6,17 +6,23 @@
 /*   By: mcossu <mcossu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/02 14:30:44 by mcossu            #+#    #+#             */
-/*   Updated: 2021/05/02 14:30:45 by mcossu           ###   ########.fr       */
+/*   Updated: 2021/05/04 18:36:05 by mcossu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Replacer.hpp"
-
+#include <iostream>
+#include <unistd.h>
+#include <time.h>
 void	Replacer::replace()
 {
 	size_t it;
-	while ((it = _str.find(_s1)) != std::string::npos)
+	it = _str.find(_s1);
+	while (it != std::string::npos)
+	{
 		_str.replace(it, _s1.length(), _s2);
+		it = _str.find(_s1, it + _s2.length());
+	}
 }
 Replacer::Replacer(const std::string &str, const std::string &s1, const std::string &s2) : _old_str(str), _str(str), _s1(s1), _s2(s2)
 {
