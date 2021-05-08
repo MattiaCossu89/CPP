af1245b84dec2787b0985b7bd04253407a21d5e0
Mattia Cossu <mcossu@e3r8p5.42roma.it>
2021-05-08 15:45:48 +0200

    fix

--- 3/ex00/main.cpp
+++ 3/ex00/main.cpp
@@ -6,13 +6,11 @@
 /*   By: mcossu <mcossu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/02 14:36:22 by mcossu            #+#    #+#             */
-/*   Updated: 2021/05/03 16:44:13 by mcossu           ###   ########.fr       */
+/*   Updated: 2021/05/08 15:45:41 by mcossu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "FragTrap.hpp"
-
-typedef void (FragTrap::*fun)(ClapTrap &) const;
 
 int main()
 {
