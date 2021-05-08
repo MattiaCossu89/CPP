d5a354c6df9cc66ce8728b2d0f9652543a44ffd6
mattia <isao@hotmail.it>
2021-05-08 02:29:58 +0200

    ex02 finish

---
+++ 7/ex02/Array.hpp
@@ -0,0 +1,83 @@
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+#define UI unsigned int
+#include <exception>
+#include <iostream>
+
+template<class T>
+class Array
+{
+	private:
+		UI _size;
+		T *_arr;
+	public:
+		Array();
+		Array(UI n);
+		Array(const Array &copy);
+		~Array();
+		Array	&operator=(const Array &copy);
+		T		&operator[](UI pos);
+		UI		size() const;
+};
+
+template<class T>
+Array<T>::Array()
+{
+	_size = 0;
+	_arr = new T[0];
+}
+
+template<class T>
+Array<T>::Array(UI n)
+{
+	_size = n;
+	_arr = new T[_size];
+}
+
+
+template<class T>
+Array<T>::Array(const Array<T> &copy) : _size(copy._size)
+{
+	_arr = new T[_size];
+	for (UI i = 0; i < _size; i++)
+		_arr[i] = copy._arr[i];
+}
+
+template<class T>
+Array<T> &Array<T>::operator=(const Array<T> &copy)
+{
+	if (this == &copy)
+		return (*this);
+	if (_arr)
+		delete [] _arr;
+	_size = copy._size;
+	_arr = new T[_size];
+	for (UI i = 0; i < _size; i++)
+		_arr[i] = copy._arr[i];
+	return (*this);
+}
+
+template<class T>
+Array<T>::~Array()
+{
+	delete [] _arr;
+	_arr = 0;
+	_size = 0;
+}
+
+template<class T>
+T &Array<T>::operator[](UI pos)
+{
+	if (pos >= _size)
+		throw std::exception();
+	return (_arr[pos]);
+}
+
+template<class T>
+UI Array<T>::size() const
+{
+	return (_size);
+}
+
+#endif
---
+++ 7/ex02/main.cpp
@@ -0,0 +1,76 @@
+#include "Array.hpp"
+#include "colors.hpp"
+
+#include <stdlib.h>
+
+#include <ctime>
+#include <iostream>
+
+void	upperString(std::string & str)
+{
+	for (size_t i = 0; i < str.length(); i++)
+		str[i] = toupper(str[i]);
+}
+
+void	incInt(int &num)
+{
+	num++;
+}
+
+template<class T>
+void iter(Array<T> &ar, void (*fun)(T&))
+{
+	std::cout << RED << ar.size() << NRM << std::endl;
+	for (UI i = 0; i < ar.size(); i++)
+		(*fun)(ar[i]);
+}
+
+template<class T>
+void printArray(Array<T> &ar)
+{
+	std::cout << "[";
+	for (size_t i = 0; i < ar.size(); i++)
+	{
+		std::cout << ar[i];
+		if (i != ar.size() - 1)
+			std::cout << ", ";
+	}
+	std::cout << "]" << std::endl;
+}
+
+int main()
+{
+	srand(time(NULL));
+	Array<int> arr(10), arr2;
+
+	for (UI i = 0; i < arr.size(); i++)
+		arr[i] = rand() % 100;
+	arr2 = arr;
+	std::cout << YELLOW "First array:" NRM << std::endl;
+	printArray(arr);
+	std::cout << YELLOW "Second array:" NRM << std::endl;
+	printArray(arr2);
+	iter(arr2, &incInt);
+	std::cout << GREEN "Arrays after second's edit:" NRM << std::endl;
+	std::cout << YELLOW "First array:" NRM << std::endl;
+	printArray(arr);
+	std::cout << YELLOW "Second array:" NRM << std::endl;
+	printArray(arr2);
+
+	Array<std::string> strs(3);
+	Array<std::string> strs2;
+	strs[0] = "Ehila";
+	strs[1] = "Hola";
+	strs[2] = "Strings are so beautiful!!";
+	strs2 = strs;
+	std::cout << YELLOW "First string array:" NRM << std::endl;
+	printArray(strs);
+	std::cout << YELLOW "Second string array:" NRM << std::endl;
+	printArray(strs2);
+	iter(strs2, &upperString);
+	std::cout << GREEN "Arrays after second's edit:" NRM << std::endl;
+	std::cout << YELLOW "First string array:" NRM << std::endl;
+	printArray(strs);
+	std::cout << YELLOW "Second string array:" NRM << std::endl;
+	printArray(strs2);
+}
