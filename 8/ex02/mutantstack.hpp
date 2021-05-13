8426ac735150e486a2a97c347f68e0a01fa3fef7
Mattia Cossu <mcossu@e3r8p5.42roma.it>
2021-05-13 19:23:11 +0200

    ok

--- 8/ex02/mutantstack.hpp
+++ 8/ex02/mutantstack.hpp
@@ -4,11 +4,11 @@
 #include <deque>
 #include <stack>
 
-template<class T>
+template<class T, class _Container = std::deque<T> >
 class stack_iterator
 {
 	private:
-		typedef typename std::deque<T>::reverse_iterator iter;
+		typedef typename _Container::reverse_iterator iter;
 		iter it;
 	public:
 		stack_iterator();
@@ -24,19 +24,131 @@ class stack_iterator
 		T &operator*();
 };
 
-template<class T>
-class MutantStack : public std::stack<T>
+template<class T, class _Container = std::deque<T> >
+class MutantStack : public std::stack<T, _Container>
 {
 	public:
 
-		typedef typename ::stack_iterator<T>	iterator;
+		typedef typename ::stack_iterator<T, _Container>	iterator;
 		MutantStack();
-		MutantStack(const std::stack<T> &copy);
+		MutantStack(const std::stack<T, _Container> &copy);
 		MutantStack(const MutantStack & copy);
 		MutantStack &operator=(const MutantStack &copy);
 
 		iterator begin();
 		iterator end();
 };
+
+
+
+template<class T, class _Container>
+stack_iterator<T, _Container>::stack_iterator()
+{
+
+}
+
+template<class T, class _Container>
+stack_iterator<T, _Container>::stack_iterator(const stack_iterator<T, _Container> &in)
+{
+	it = in.it;
+}
+
+template<class T, class _Container>
+stack_iterator<T, _Container>::stack_iterator(const iter &in)
+{
+	it = in;
+}
+
+template<class T, class _Container>
+stack_iterator<T, _Container> &stack_iterator<T, _Container>::operator=(const stack_iterator<T, _Container> &copy)
+{
+	it = copy.it;
+}
+
+template<class T, class _Container>
+bool stack_iterator<T, _Container>::operator==(const stack_iterator<T, _Container> &it) const
+{
+	return (this->it == it.it);
+}
+
+template<class T, class _Container>
+bool stack_iterator<T, _Container>::operator!=(const stack_iterator<T, _Container> &it) const
+{
+	return !(this->it == it.it);
+}
+
+template<class T, class _Container>
+stack_iterator<T, _Container> &stack_iterator<T, _Container>::operator++()
+{
+	it++;
+	return (*this);
+}
+
+template<class T, class _Container>
+const stack_iterator<T, _Container> stack_iterator<T, _Container>::operator++(int)
+{
+	stack_iterator<T, _Container> n = *this;
+	it++;
+	return (n);
+}
+
+template<class T, class _Container>
+stack_iterator<T, _Container> &stack_iterator<T, _Container>::operator--()
+{
+	it--;
+	return (*this);
+}
+
+template<class T, class _Container>
+const stack_iterator<T, _Container> stack_iterator<T, _Container>::operator--(int)
+{
+	stack_iterator<T, _Container> n = *this;
+	it--;
+	return (n);
+}
+
+template<class T, class _Container>
+T &stack_iterator<T, _Container>::operator*()
+{
+	return (*it);
+}
+
+
+template<class T, class _Container>
+typename MutantStack<T, _Container>::iterator MutantStack<T, _Container>::begin()
+{
+	return this->c.rbegin();
+}
+
+template<class T, class _Container>
+typename MutantStack<T, _Container>::iterator MutantStack<T, _Container>::end()
+{
+	return this->c.rend();
+}
+template<class T, class _Container>
+MutantStack<T, _Container>::MutantStack() : std::stack<T>()
+{
+
+}
+
+template<class T, class _Container>
+MutantStack<T, _Container>::MutantStack(const std::stack<T, _Container> &copy) : std::stack<T, _Container>(copy)
+{
+
+}
+
+template<class T, class _Container>
+MutantStack<T, _Container>::MutantStack(const MutantStack<T, _Container> & copy) : std::stack<T, _Container>(copy)
+{
+
+}
+
+template<class T, class _Container>
+MutantStack<T, _Container> &MutantStack<T, _Container>::operator=(const MutantStack<T, _Container> &copy)
+{
+	this->std::stack<T>::operator=(copy);
+	return (*this);
+}
+
 
 #endif
