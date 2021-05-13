690997e8434caafeb4af62d13b3f906b59a7c9b6
Mattia Cossu <mcossu@e3r8p5.42roma.it>
2021-05-13 19:39:31 +0200

    queue is impossible

--- 8/ex02/main.cpp
+++ 8/ex02/main.cpp
@@ -1,16 +1,17 @@
 #include "mutantstack.hpp"
 #include <iostream>
 #include <list>
+#include <vector>
 int main()
 {
-	MutantStack<int, std::list<int> > m;
-	std::stack<int, std::list<int> > stack_;
+	MutantStack<int, std::vector<int> > m;
+	std::stack<int, std::vector<int> > stack_;
 	m = stack_;
 	for (int i = 0; i < 10; i++)
 		m.push(i);
-	MutantStack<int, std::list<int> >::iterator it = m.begin();
+	MutantStack<int, std::vector<int> >::iterator it = m.begin();
 	for (; it != m.end(); it++)
 		std::cout << *it << " ";
 	std::cout << std::endl;
-	std::stack<int, std::list<int> > s(m);
+	std::stack<int, std::vector<int> > s(m);
 }
